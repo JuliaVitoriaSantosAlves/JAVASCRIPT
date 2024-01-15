f47c4f4f85ef409db1d4ba860204896c4fcbefd3
QEIO <vitti2vita@gmail.com>
2024-01-15 19:39:38 -0300

    aula de continue.js concluida

---
+++ 01.15/continue.js
@@ -0,0 +1,9 @@
+let listaNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < listaNum.length; i++) {
+    console.log(listaNum[i]);
+    if (i == 5) {
+        break
+    }
+    console.log(listaNum[i]);
+}
