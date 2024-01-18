55edfa77b6ce3472259f1de9782b0275547b1e54
QEIO <vitti2vita@gmail.com>
2024-01-18 18:33:00 -0300

    erro da questao01.js corrigido

--- 01.17/questao01.js
+++ 01.17/questao01.js
@@ -1,22 +1,21 @@
 const prompt = require('prompt-sync')();
 
 function formatDate() {
-    const dateString = prompt(`Informe uma data: `);
-    const dateParts = dateString.split('/');
-    const dia = parseInt(dateParts[0]);
-    const mes = parseInt(dateParts[1]);
-    const ano = parseInt(dateParts[2]);
-  
-    const meses = [
-      'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
-      'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
-    ];
-  
-    const date = new Date(ano, mes - 1, dia);
-    const mesName = meses[(date.mes)];
-    const anoString = ano;
-  
-    return `${dia} de ${mesName} de ${anoString}`;
-  }
+  const dateString = prompt(`Informe uma data: `);
+  const dateParts = dateString.split('/');
+  const dia = parseInt(dateParts[0]);
+  const mes = parseInt(dateParts[1]);
+  const ano = parseInt(dateParts[2]);
+
+  const meses = [
+    'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+    'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
+  ];
+
+  const mesName = meses[mes-1];
+  const anoString = ano;
+
+  return `${dia} de ${mesName} de ${anoString}`;
+}
 
 console.log(`Sua data é ${formatDate()}`);
