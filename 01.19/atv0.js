91600c3ff8567a51e8ec1095ae3d568b897a21ae
QEIO <vitti2vita@gmail.com>
2024-01-19 22:43:10 -0300

    Aula concuida

---
+++ 01.19/atv0.js
@@ -0,0 +1,8 @@
+/* um codigo que indique como, quantas e quais cedulas são necessarias para entregar um valor determinado.
+
+cedulas: 2, 5, 10, 20, 50, 100 e 200.
+    else: valor não fecha em cedulas.
+*/
+
+const prompt = require('prompt-sync')();
+
