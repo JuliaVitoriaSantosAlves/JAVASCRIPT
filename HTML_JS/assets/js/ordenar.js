4e5b315a4b02f04088a17932b4fe5aed7fe4fcc6
QEIO <vitti2vita@gmail.com>
2024-01-24 00:22:10 -0300

    ordenar totalmente funcional

--- HTML_JS/assets/js/ordenar.js
+++ HTML_JS/assets/js/ordenar.js
@@ -1,7 +1,20 @@
+let lista = [];
+
 function adicionarValor() {
-    
+    let valor = document.getElementById("input").value;
+    lista.push(valor);
+    document.getElementById("input").value = "";
 }
 
 function ordenarValor() {
+    lista.sort();
+    let valores = lista.join(", ");
+    let listaElement = document.getElementById("lista");
+    listaElement.innerHTML = "";
 
+    for (let i = 0; i < lista.length; i++) {
+        let item = document.createElement("li");
+        item.innerText = lista[i];
+        listaElement.appendChild(item);
+    }
 }
