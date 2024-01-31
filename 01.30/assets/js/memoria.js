28d139f319586ba4f262d2525b5750321610b041
QEIO <vitti2vita@gmail.com>
2024-01-30 21:33:57 -0300

    PROJETO NÃO CONCLUIDO

---
+++ 01.30/assets/js/memoria.js
@@ -0,0 +1,10 @@
+const cartas = [1, 1, 2, 2, 3, 3, 4, 4];
+
+async function geraImagemPar(){
+    const imagemPar = {};
+    for (let i = 0; i < cartas.length; i++) {
+        if (!imagemPar[cartas[]]) {
+            const id = 
+        }
+    }
+}
