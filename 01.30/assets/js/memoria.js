34be957511cefd1dce8881953378d29ff934f974
QEIO <vitti2vita@gmail.com>
2024-01-30 21:51:13 -0300

    relogio.html/css/js concluido

--- 01.30/assets/js/memoria.js
+++ 01.30/assets/js/memoria.js
@@ -3,8 +3,8 @@ const cartas = [1, 1, 2, 2, 3, 3, 4, 4];
 async function geraImagemPar(){
     const imagemPar = {};
     for (let i = 0; i < cartas.length; i++) {
-        if (!imagemPar[cartas[]]) {
-            const id = 
+        if (!imagemPar[cartas]) {
+            const id = s
         }
     }
 }
